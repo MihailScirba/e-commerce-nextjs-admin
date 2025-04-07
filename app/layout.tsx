06d78c96271dd36d8de2ff1f4c065e1937c4c453
MihailScirba <mihailscirba8@gmail.com>
2025-04-07 12:18:37 +0300

    Finished up set up of database interaction. Instaled and configured prisma to a cloud database. Figured out how does work the axios library and prisma. Created stores entity and post method for creating new stores.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,42 +1,47 @@
-import { type Metadata } from 'next'
+import { type Metadata } from "next";
 import {
   ClerkProvider,
   SignInButton,
+  SignOutButton,
   SignUpButton,
   SignedIn,
   SignedOut,
   UserButton,
-} from '@clerk/nextjs'
-import { Geist, Geist_Mono } from 'next/font/google'
+} from "@clerk/nextjs";
+import { Geist, Geist_Mono } from "next/font/google";
 
-import './globals.css'
-import { ModalProvider } from '@/providers/modal-provider'
+import "./globals.css";
+import { ModalProvider } from "@/providers/modal-provider";
+import prismadb from "@/lib/prismadb";
+import { ToasterProvider } from "@/providers/toast-provider";
 
 const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-})
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
 
 const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-})
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
-  title: 'Ecommerce Admin Dashboard',
-  description: 'Ecommerce Admin Dashboard',
-}
+  title: "Ecommerce Admin Dashboard",
+  description: "Ecommerce Admin Dashboard",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-          {/* <header className="flex justify-end items-center p-4 gap-4 h-16">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <header className="flex justify-end items-center p-4 gap-4 h-16">
             <SignedOut>
               <SignInButton />
               <SignUpButton />
@@ -44,11 +49,14 @@ export default function RootLayout({
             <SignedIn>
               <UserButton />
             </SignedIn>
-          </header> */}
-          <ModalProvider />
+          </header>
+          <SignedIn>
+            <ToasterProvider />
+            <ModalProvider />
+          </SignedIn>
           {children}
         </body>
       </html>
     </ClerkProvider>
-  )
+  );
 }
