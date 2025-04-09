876df659165cfa7c51efeb212280eaec169dd0d0
MihailScirba <mihailscirba8@gmail.com>
2025-04-09 18:25:28 +0300

    I finished up the settings page. Implemented the delete(http:delete) and update(http:patch) for store entity. Added api route to make connection with fronted of user store application in the future. Added new components from shadcn/ui. And implemented a handsome functionality of copying the api to the clipboard.

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -10,49 +10,138 @@ import { useForm } from "react-hook-form";
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useState } from "react";
-import { Form } from "@/components/ui/form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import toast from "react-hot-toast";
+import axios from "axios";
+import { useParams, useRouter } from "next/navigation";
+import AlertModal from "@/components/modals/alert-modal";
+import ApiAlert from "@/components/ui/api-alert";
 
 interface SettingsFormProps {
   initialData: Store;
 }
 
 const formSchema = z.object({
-  name: z.string().min(1)
-})
+  name: z.string().min(1),
+});
 
-type settingsFormValues = z.infer<typeof formSchema>;
+type SettingsFormValues = z.infer<typeof formSchema>;
 
 export default function SettingsForm({ initialData }: SettingsFormProps) {
-  const [open, setOpen] = useState(false)
-  const [loading, setLoading] = useState(false)
+  const router = useRouter();
+  const params = useParams();
+  const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-  const onSubmit = async (data: settingsFormValues) => {
-    console.log(data);
-  }
-
-  const form = useForm<settingsFormValues>({
+  const form = useForm<SettingsFormValues>({
     resolver: zodResolver(formSchema),
-    defaultValues: initialData
-  })
+    defaultValues: initialData,
+  });
+
+  const onSubmit = async (data: SettingsFormValues) => {
+    const { storeId } = await params;
+    try {
+      setLoading(true);
+      await axios.patch(`/api/stores/${storeId}`, data);
+      router.refresh();
+      toast.success("Store updated successfuly");
+    } catch (error) {
+      toast.error("Something went wrong");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const onDelete = async () => {
+    const { storeId } = await params;
+    try {
+      setLoading(true);
+      await axios.delete(`/api/stores/${storeId}`);
+      router.refresh();
+      router.push("/");
+      toast.success("Store deleted successfuly");
+    } catch (error) {
+      toast.error(
+        "Make sure you deleted all products and categories before / Something went wrong"
+      );
+      console.log(error);
+    } finally {
+      setLoading(false);
+      setOpen(false);
+    }
+  };
+
   return (
     <>
+      <AlertModal
+        isOpen={open}
+        onClose={() => setOpen(false)}
+        onConfirm={onDelete}
+        loading={loading}
+      />
       <div className="flex items-center justify-between">
         <Heading title="Settings" description="Manage store preferences" />
-        <Button variant="destructive" size="icon" onClick={() => {}}>
+        <Button
+          variant="destructive"
+          disabled={loading}
+          size="icon"
+          onClick={() => {
+            setOpen(true);
+          }}
+        >
           <Trash className="h-4 w-4" />
         </Button>
       </div>
       <Separator />
       <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 w-full">
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          onClick={() => console.log("Parent FORM clicked!")}
+          className="space-y-8 w-full"
+        >
           <div className="grid grid-cols-3 gap-8">
-            {/* 
-              TODO: Finish form for updating data
-              Tutorial stop -> 3:00:00
-            */}
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={loading}
+                      placeholder="Store name ..."
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
           </div>
+          <Button
+            disabled={loading}
+            className="ml-auto bg-green-800 hover:bg-green-900"
+            type="submit"
+            onClick={() => console.log("Hello click")}
+          >
+            Save changes
+          </Button>
         </form>
       </Form>
+      <Separator />
+      <ApiAlert 
+        title="NEXT_PUBLIC_API_URL" 
+        description={`${origin}/api/${params.storeId}`}
+        variant="public" 
+      />
     </>
   );
 }
